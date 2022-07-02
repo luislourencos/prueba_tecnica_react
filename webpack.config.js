a3c771d7a4c0b471fc180d63d263590c69cc7b7f
luislourencos <luis.lourencos@gmail.com>
2022-07-03 00:31:24 +0200

    add bundle js

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     entry: './src/index.js',
     mode: 'development',
     output: {
-        path: path.join(__dirname, '/dist'),
+        path: path.join(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     devServer: {
